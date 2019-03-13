2dd6a66da57e0f2581d1d6d92d3b8f1b53843858
NriosD <nelsonfabianrios@hotmail.com>
2019-03-13 11:35:11 -0500

    varios cambios

---
+++ TaskerSpaceX/TaskerSpaceX/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskerSpaceX.Models;
+
+namespace TaskerSpaceX.Data
+{
+    public class DataContext:DbContext
+    {
+
+        public DbSet<Value> Values { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+    }
+}
